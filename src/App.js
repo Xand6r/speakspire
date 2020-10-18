ed0a64811997319133ba28b7f8399b295e49e8cb
Adetunji Oluwaferanmi <oluwaferanmiadetunji@gmail.com>
2020-10-18 14:40:05 +0100

    Add lazy load

--- src/App.js
+++ src/App.js
@@ -1,42 +1,49 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import { component as Homepage } from './components/homepage';
-import { component as SpeakersPage } from './components/speakersPage';
-import { component as SignUpCategory } from './components/category';
-import { component as SignInPage } from './components/signin';
-import { component as SpeakerSignUpPage } from './components/speakerRegister';
-import { component as OrganiserSignUpPage } from './components/organiserRegister';
-import { component as EventsPage } from './components/eventsPage';
-import { component as SpeakersProfile } from './components/speakerProfile';
-import { component as EventProfile } from './components/eventProfile';
-import { component as OrganisersPage } from './components/organisersPage';
-import { component as EventSignUpPage } from './components/eventsRegister';
-import { component as OrganiserProfile } from './components/organiserProfile';
-import { component as About } from './components/about'
+import React, {Suspense, lazy} from 'react';
+import {Route, Switch} from 'react-router-dom';
+import 'antd/dist/antd.css';
 
-import IndividualSignUp from './components/individualSignup';
-
-import 'antd/dist/antd.css'; 
+const Homepage = lazy(() => import('./components/homepage' /* webpackChunkName: "Homepage" */));
+const SpeakersPage = lazy(() => import('./components/speakersPage' /* webpackChunkName: "SpeakersPage" */));
+const SignUpCategory = lazy(() => import('./components/category' /* webpackChunkName: "SignUpCategory" */));
+const SignInPage = lazy(() => import('./components/signin' /* webpackChunkName: "SignInPage" */));
+const SpeakerSignUpPage = lazy(() => import('./components/speakerRegister' /* webpackChunkName: "SpeakerSignUpPage" */));
+const OrganiserSignUpPage = lazy(() => import('./components/organiserRegister' /* webpackChunkName: "OrganiserSignUpPage" */));
+const EventsPage = lazy(() => import('./components/eventsPage' /* webpackChunkName: "EventsPage" */));
+const SpeakersProfile = lazy(() => import('./components/speakerProfile' /* webpackChunkName: "SpeakersProfile" */));
+const SpeakersProfile = lazy(() => import('./components/eventProfile' /* webpackChunkName: "SpeakersProfile" */));
+const OrganisersPage = lazy(() => import('./components/organisersPage' /* webpackChunkName: "OrganisersPage" */));
+const EventSignUpPage = lazy(() => import('./components/eventsRegister' /* webpackChunkName: "EventSignUpPage" */));
+const OrganiserProfile = lazy(() => import('./components/organiserProfile' /* webpackChunkName: "OrganiserProfile" */));
+const About = lazy(() => import('./components/about' /* webpackChunkName: "About" */));
+const IndividualSignUp = lazy(() => import('./components/individualSignup' /* webpackChunkName: "IndividualSignUp" */));
 
 function App() {
-  return (
-    <Switch>
-      <Route exact  path='/' component={Homepage} />
-      <Route exact path="/about" component={About} />
-      <Route exact path='/speakers' component={SpeakersPage} />
-      <Route exact path='/events' component={EventsPage} />
-      <Route exact path='/category' component={SignUpCategory} />
-      <Route exact path='/login' component={SignInPage} />
-      <Route path='/register' component={SpeakerSignUpPage} />
-      <Route path='/organiser' component={OrganiserSignUpPage} />
-      <Route path='/profile' component={SpeakersProfile} />
-      <Route path='/eventprofile' component={EventProfile} />
-      <Route path='/organisers' component={OrganisersPage} />
-      <Route path='/individual' component={IndividualSignUp} />
-      <Route path="/registerevent" component={EventSignUpPage} />
-      <Route path='/organiserprofile' component={OrganiserProfile} />
-    </Switch>
-  );
+	return (
+		<Suspense
+			fallback={
+				<div>
+					Loading...
+				</div>
+			}
+		>
+			<Switch>
+				<Route exact path='/' component={Homepage} />
+				<Route exact path='/about' component={About} />
+				<Route exact path='/speakers' component={SpeakersPage} />
+				<Route exact path='/events' component={EventsPage} />
+				<Route exact path='/category' component={SignUpCategory} />
+				<Route exact path='/login' component={SignInPage} />
+				<Route path='/register' component={SpeakerSignUpPage} />
+				<Route path='/organiser' component={OrganiserSignUpPage} />
+				<Route path='/profile' component={SpeakersProfile} />
+				<Route path='/eventprofile' component={EventProfile} />
+				<Route path='/organisers' component={OrganisersPage} />
+				<Route path='/individual' component={IndividualSignUp} />
+				<Route path='/registerevent' component={EventSignUpPage} />
+				<Route path='/organiserprofile' component={OrganiserProfile} />
+			</Switch>
+		</Suspense>
+	);
 }
 
 export default App;
