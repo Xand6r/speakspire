b67c41bda566f6d09eb8b976a609b0d16b4df386
Adetunji Oluwaferanmi <oluwaferanmiadetunji@gmail.com>
2020-10-26 10:29:09 +0100

    Add single event page

--- src/App.js
+++ src/App.js
@@ -6,13 +6,12 @@ import 'antd/dist/antd.css';
 
 const Homepage = lazy(() => import('./components/homepage' /* webpackChunkName: "Homepage" */));
 const SpeakersPage = lazy(() => import('./components/speakersPage' /* webpackChunkName: "SpeakersPage" */));
-const SingleSpeakerPage = lazy(() => import('./components/singleSpeakerProfile' /* webpackChunkName: "SingleSpeakerPage" */));
+const SpeakersProfile = lazy(() => import('./components/speakerProfile' /* webpackChunkName: "SingleSpeakerPage" */));
 const SignUpCategory = lazy(() => import('./components/category' /* webpackChunkName: "SignUpCategory" */));
 const SignInPage = lazy(() => import('./components/signin' /* webpackChunkName: "SignInPage" */));
 const SpeakerSignUpPage = lazy(() => import('./components/speakerRegister' /* webpackChunkName: "SpeakerSignUpPage" */));
 const OrganiserSignUpPage = lazy(() => import('./components/organiserRegister' /* webpackChunkName: "OrganiserSignUpPage" */));
 const EventsPage = lazy(() => import('./components/eventsPage' /* webpackChunkName: "EventsPage" */));
-const SpeakersProfile = lazy(() => import('./components/speakerProfile' /* webpackChunkName: "SpeakersProfile" */));
 const EventProfile = lazy(() => import('./components/eventProfile' /* webpackChunkName: "EventProfile" */));
 const OrganisersPage = lazy(() => import('./components/organisersPage' /* webpackChunkName: "OrganisersPage" */));
 const EventSignUpPage = lazy(() => import('./components/eventsRegister' /* webpackChunkName: "EventSignUpPage" */));
@@ -21,8 +20,8 @@ const About = lazy(() => import('./components/about' /* webpackChunkName: "About
 const IndividualSignUp = lazy(() => import('./components/individualSignup' /* webpackChunkName: "IndividualSignUp" */));
 
 const ProtectedRoute = ({ component: Component, ...rest }) => {
-	const { loggedIn } = useSelector(({ user }) => user);
-
+	// const { loggedIn } = useSelector(({ user }) => user);
+	const loggedIn = true;
 	return <Route {...rest} render={(props) => (loggedIn ? <Component {...props} /> : <Redirect to={{ pathname: '/login' }} />)} />;
 };
 
@@ -32,18 +31,17 @@ function App() {
 			<Switch>
 				<Route exact path='/about' component={About} />
 				<ProtectedRoute exact path='/speakers' component={SpeakersPage} />
-				<Route exact path='/speakers/:id' component={SingleSpeakerPage} />
+				<ProtectedRoute exact path='/speakers/:id' component={SpeakersProfile} />
 				<ProtectedRoute exact path='/events' component={EventsPage} />
 				<Route exact path='/category' component={SignUpCategory} />
 				<Route exact path='/login' component={SignInPage} />
 				<Route path='/register' component={SpeakerSignUpPage} />
 				<Route path='/organiser' component={OrganiserSignUpPage} />
-				<ProtectedRoute path='/profile' component={SpeakersProfile} />
-				<ProtectedRoute path='/eventprofile' component={EventProfile} />
+				<ProtectedRoute path='/events/:id' component={EventProfile} />
 				<ProtectedRoute path='/organisers' component={OrganisersPage} />
 				<Route path='/individual' component={IndividualSignUp} />
 				<Route path='/registerevent' component={EventSignUpPage} />
-				<Route path='/organiserprofile/:id' component={OrganiserProfile} />
+				<ProtectedRoute path='/organiserprofile/:id' component={OrganiserProfile} />
 				<Route exact path='/' component={Homepage} />
 			</Switch>
 		</Suspense>
--- src/components/eventProfile/subcomponents/profileCard/profilecard.js
+++ src/components/eventProfile/subcomponents/profileCard/profilecard.js
@@ -4,8 +4,7 @@ import ellipsisIcon from '../../assets/ellipsis.svg';
 import shareIcon from '../../assets/share.svg';
 import profileIcon from '../../assets/playIcon.svg';
 import playIcon from '../../assets/profileIcon.svg';
-import blueSpeaker from '../../assets/blueSpeaker.svg'
-
+import blueSpeaker from '../../assets/blueSpeaker.svg';
 
 import globeIcon from '../../assets/globe.svg';
 import locationIcon from '../../assets/location.svg';
@@ -14,76 +13,63 @@ import calendar from '../../assets/calendar.svg';
 import linkchain from '../../assets/linkchain.svg';
 import organiserplaceholder from '../../assets/organiserplaceholder.svg';
 
-
 import './profilecard.scss';
 
-const tag="premium"
-export default function Profilecard() {
-    return (
-        <div class="events_profilecard">
+const tag = 'premium';
+export default function Profilecard({ userData }) {
+	const { name, organizer, schedule, language } = userData;
+	return (
+		<div class='events_profilecard'>
+			<div className='events_profilecard__actions'>
+				<img src={shareIcon} alt='share' />
+				<img src={ellipsisIcon} alt='ellipsis' />
+			</div>
 
-            <div className="events_profilecard__actions">
-                <img src={shareIcon} alt="share"/>
-                <img src={ellipsisIcon} alt="ellipsis"/>
-            </div>
+			<div className='events_profilecard__maincontent'>
+				<div className='events_profilecard__maincontent__left'>
+					<div className='--event_brief'>
+						<img src={organiserplaceholder} alt='' />
+						<span>{organizer}</span>
+					</div>
 
-            <div className="events_profilecard__maincontent">
-                <div className="events_profilecard__maincontent__left">
+					<div className='--event_description'>{name}</div>
 
-                    <div className="--event_brief">
-                        <img src={organiserplaceholder} alt=""/>
-                        <span>Accenture</span>
-                    </div>
+					<div className='--event_theme'>
+						<div className='--themeheading'>Theme</div>
+						<div className='--themecontent'>The Role of Media in Growing the Economy</div>
+					</div>
+				</div>
 
-                    <div className="--event_description">
-                        Accenture Fashion, Arts, Design & Creativity Festival 2020
-                    </div>
+				<div className='events_profilecard__maincontent__right'>
+					<div className='events_profilecard__maincontent__right__item'>
+						<img className='--icon' src={calendar} alt='' />
+						<div className='--text'>Tue, Jan 20, 2020 10:00PM</div>
+					</div>
 
-                    <div className="--event_theme">
-                        <div className="--themeheading">
-                            Theme
-                        </div>
-                        <div className="--themecontent">
-                            The Role of Media in Growing the Economy
-                        </div>
-                    </div>
-                </div>
+					<div className='events_profilecard__maincontent__right__item'>
+						<img className='--icon' src={locationIcon} alt='' />
+						<div className='--text'>{schedule ? schedule[0].onlineLink : null}</div>
+					</div>
 
+					<div className='events_profilecard__maincontent__right__item'>
+						<img className='--icon' src={linkchain} alt='' />
+						<div className='--text'>zoom.us/conference</div>
+					</div>
 
-                <div className="events_profilecard__maincontent__right">
+					<div className='events_profilecard__maincontent__right__item'>
+						<img className='--icon' src={globeIcon} alt='' />
+						<div className='--text'>{language}</div>
+					</div>
 
-                    <div className="events_profilecard__maincontent__right__item">
-                        <img className="--icon" src={calendar} alt="" />
-                        <div className="--text">Tue, Jan 20, 2020 10:00PM</div>
-                    </div>
-
-                    <div className="events_profilecard__maincontent__right__item">
-                        <img className="--icon" src={locationIcon} alt="" />
-                        <div className="--text">Eko Hotels & Suites, Lagos, Nigeria</div>
-                    </div>
-
-                    <div className="events_profilecard__maincontent__right__item">
-                        <img className="--icon" src={linkchain} alt="" />
-                        <div className="--text">zoom.us/conference</div>
-                    </div>
-
-                    <div className="events_profilecard__maincontent__right__item">
-                        <img className="--icon" src={globeIcon} alt="" />
-                        <div className="--text">English, French</div>
-                    </div>
-
-                    <div className="events_profilecard__maincontent__right__item --top">
-                        <div className="--speakercall">
-                            <img src={blueSpeaker} alt="bluespeaker"/>
-                            <span>Call for Speakers</span>
-                        </div>
-                        <div className="--speakercall_action">
-                            View Details
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-    )
+					<div className='events_profilecard__maincontent__right__item --top'>
+						<div className='--speakercall'>
+							<img src={blueSpeaker} alt='bluespeaker' />
+							<span>Call for Speakers</span>
+						</div>
+						<div className='--speakercall_action'>View Details</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
