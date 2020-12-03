6a0eabaf1ccc553bbad03658d8a408a4d876fc25
oluwaferanmiadetunji <oluwaferanmiadetunji@gmail.com>
2020-12-03 22:38:44 +0100

    Add topic area edit

---
+++ src/utilities/updates/speakerTopicUpdate/validator.js
@@ -0,0 +1,33 @@
+const cleanData = (data) => {
+	const primary_specialty = data.primarySpecialty;
+	const secondary_specialty = data.secondarySpecialty;
+	const primary_topic = data.primaryTopicArea;
+	const secondary_topic = data.secondaryTopicArea;
+	const primary_tags = JSON.stringify(data.primary_topic_tags);
+	const secondary_tags = JSON.stringify(data.secondary_topic_tags);
+
+	return Object.freeze({
+		primary_specialty,
+		secondary_specialty,
+		primary_topic,
+		secondary_topic,
+		primary_tags,
+		secondary_tags,
+	});
+};
+
+const validateData = (data) => {
+	let valid = false;
+	const primary_specialty = data.primarySpecialty;
+	const primary_topic = data.primaryTopicArea;
+	const primary_tags = data.primary_topic_tags;
+	if (primary_specialty.trim() === '' || primary_topic.trim() === '' || primary_tags.length < 1) {
+		valid = false;
+	} else {
+		valid = true;
+	}
+
+	return valid;
+};
+
+export { cleanData, validateData };
